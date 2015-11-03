3ae92e1225c3481f0e848c633e4cef667abaf8ce
Hardik Mehta <hardik9720@users.noreply.github.com>
2015-11-03 15:20:05 +0530

    Create mailgapform.js
    created file mailgapform.js

---
+++ client/js/mailgapform.js
@@ -0,0 +1,88 @@
+angular.module('nibs.mailgapform', ['openfb', 'nibs.status', 'nibs.activity', 'nibs.wishlist'])
+
+    .config(function ($stateProvider) {
+
+        $stateProvider
+
+            .state('app.mailgapform', {
+                url: "/mailgapform",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/mailgapform.html",
+                        controller: "MailGapCtrl"
+                    }
+                }
+            })
+
+    })
+
+    // REST resource for access to Products data
+    .factory('Mailgap', function ($http, $rootScope) {
+        return {
+            mailgapsubmit: function(mailgappform) {
+                return $http.get($rootScope.server.url + '/mailgapp',mailgappform);
+            }
+        };
+    })
+
+    .controller('MailGapCtrl', function ($scope,$ionicPopup,Mailgap) {
+          $scope.mailgapform = {};
+//        Product.all().success(function(products) {
+//            $scope.products = products;
+//        });
+
+        $scope.mailgapformclick = function() {
+               Mailgap.mailgapsubmit($scope.mailgapform)
+                   .success(function (data) {
+                        //$state.go("app.profile");
+                        $ionicPopup.alert({title: 'Success', content: "The Mailgapp form submitted"});
+                   })
+                   .error(function () {
+                         $ionicPopup.alert({title: 'Oops', content: 'There is some error'});
+               });
+
+        }
+
+    });
+
+//    .controller('ProductDetailCtrl', function ($scope, $rootScope, $stateParams, $ionicPopup, Product, OpenFB, WishListItem, Activity, Status) {
+//
+//        Product.get($stateParams.productId).success(function(product) {
+//            $scope.product = product;
+//        });
+//
+//        $scope.shareOnFacebook = function () {
+//            Status.show('Shared on Facebook!');
+//            Activity.create({type: "Shared on Facebook", points: 1000, productId: $scope.product.sfid, name: $scope.product.name, image: $scope.product.image})
+//                .success(function(status) {
+//                    Status.checkStatus(status);
+//                });
+//        };
+//
+//        $scope.shareOnTwitter = function () {
+//            Status.show('Shared on Twitter!');
+//            Activity.create({type: "Shared on Twitter", points: 1000, productId: $scope.product.sfid, name: $scope.product.name, image: $scope.product.image})
+//                .success(function(status) {
+//                    Status.checkStatus(status);
+//                });
+//        };
+//
+//        $scope.shareOnGoogle = function () {
+//            Status.show('Shared on Google+!');
+//            Activity.create({type: "Shared on Google+", points: 1000, productId: $scope.product.sfid, name: $scope.product.name, image: $scope.product.image})
+//                .success(function(status) {
+//                    Status.checkStatus(status);
+//                });
+//        };
+//
+//        $scope.saveToWishList = function () {
+//            WishListItem.create({productId: $scope.product.id}).success(function(status) {
+//                Status.show('Added to your wish list!');
+//                Activity.create({type: "Added to Wish List", points: 1000, productId: $scope.product.sfid, name: $scope.product.name, image: $scope.product.image})
+//                    .success(function(status) {
+//                        Status.checkStatus(status);
+//                    });
+//            });
+//        };
+//
+//    });
