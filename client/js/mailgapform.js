e6c86a7462beab0b174f3b87a53345a314a243ae
Hardik Mehta <hardik9720@users.noreply.github.com>
2015-11-06 17:20:16 +0530

    Update mailgapform.js

--- client/js/mailgapform.js
+++ client/js/mailgapform.js
@@ -84,39 +84,31 @@ angular.module('nibs.mailgapform', ['openfb', 'nibs.status', 'nibs.activity', 'n
     	 a4  =[ 595.28,  841.89];  // for a4 size paper width and height
     	function createPDF(){
     		alert('in createpdf');
-    		
-    		html2canvas(form , {
-    	        onrendered: function( canvas ) {
-    	            var img = canvas.toDataURL("image/png"),
-    	    		  doc = new jsPDF({
-    	    		          unit:'px', 
-    	    		          format:'a4'
-    	    		        });   
-    	    		  alert('after jsPDF')
-    	    		        doc.addImage(img, 'JPEG', 20, 20);
-    	    		        doc.save('techumber-html-to-pdf.pdf');
-    	    		        alert('file downloaded done');
-    	    		        form.width(cache_width);
-    	        }
-    	    });
+    		 getCanvas().then(function(canvas){
+    			 alert('in createdpdf getcanvas');
+    		  var img = canvas.toDataURL("image/png"),
+    		  doc = new jsPDF({
+    		          unit:'px', 
+    		          format:'a4'
+    		        });   
+    		  alert('after jsPDF')
+    		        doc.addImage(img, 'JPEG', 20, 20);
+    		        doc.save('techumber-html-to-pdf.pdf');
+    		        alert('file downloaded done');
+    		        form.width(cache_width);
+    		 });
     	}
     		 
     		// create canvas object
-   	// 	function getCanvas(){
-   	// 		alert('in getcanvas');
-    // 		 form.width((a4[0]*1.33333) -80).css('max-width','none');
-    // 		 alert('after form width');
-    // 		 var x=html2canvas(form,{
-    // 		        imageTimeout:2000,
-    // 		        removeContainer:true
-    // 		    }); 
-    // 		 alert('canvas value'+x);   
-    // // 		 return html2canvas(form,{
-    // // 		     imageTimeout:2000,
-    // // 		     removeContainer:true
-    // // 		    }); 
-    // 		    return x;
-   	// 	}
+   		function getCanvas(){
+   			alert('in getcanvas');
+    		 form.width((a4[0]*1.33333) -80).css('max-width','none');
+    		 alert('after form width');
+    		 return html2canvas(form,{
+    		     imageTimeout:500,
+    		     
+    		    }); 
+   		}
     	
     	$scope.download=function(){
     		createPDF();
